26b395e5e0fdbb238b03a05dc891e1d33e983d7c
lastunicorn <coldblackstar@yahoo.com>
2017-06-02 09:40:45 +0300

    created a custom checkbox.

---
+++ sources/SomeData/HousePart.cs
@@ -0,0 +1,117 @@
+﻿// FlagCalculator
+// Copyright (C) 2017 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+
+namespace DustInTheWind.SomeData
+{
+    /// <summary>
+    /// Represents parts of a house. Usually types of rooms.
+    /// </summary>
+    /// <remarks>
+    /// It is a public enum that has the <see cref="FlagsAttribute"/> on it.
+    /// FlagCalculator application should load this enum without any problem.
+    /// </remarks>
+    [Flags]
+    public enum HousePart
+    {
+        /// <summary>
+        /// Represents no house part.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// A room used for sleeping in.
+        /// </summary>
+        Bedroom = 1,
+
+        /// <summary>
+        ///  UK also sitting room, AUSTRALIAN ENGLISH also lounge room.
+        ///  The room in a house or apartment that is used for relaxing, and entertaining guests, but not usually for eating.
+        /// </summary>
+        LivingRoom = 1 << 2,
+
+        /// <summary>
+        /// A room with a bath and/or shower and often a toilet.
+        /// </summary>
+        Bathroom = 1 << 3,
+
+        /// <summary>
+        /// The area just inside the main entrance of a house, apartment or other building which leads to other rooms and usually to the stairs.
+        /// </summary>
+        Hall = 1 << 4,
+
+        /// <summary>
+        /// Usually just for washing machine, freezer etc.
+        /// </summary>
+        UtilityRoom = 1 << 5,
+
+        /// <summary>
+        /// A separated building from the house usually for storing garden tools.
+        /// </summary>
+        Shed = 1 << 6,
+
+        /// <summary>
+        /// Space in the roof of the house usually used only for storage.
+        /// </summary>
+        Loft = 1 << 7,
+
+        /// <summary>
+        /// Room in the roof space of a house (could be lived in.)
+        /// </summary>
+        Attic = 1 << 8,
+
+        /// <summary>
+        /// Room below ground level without any windows used for storage.
+        /// </summary>
+        Cellar = 1 << 9,
+
+        /// <summary>
+        /// Room below ground level, with windows, used for living and working.
+        /// </summary>
+        Basement = 1 << 10,
+
+        /// <summary>
+        /// Flat area at the top of a staircase.
+        /// </summary>
+        Landing = 1 << 11,
+
+        /// <summary>
+        /// Covered area before the entrance door.
+        /// </summary>
+        Porch = 1 << 12,
+
+        /// <summary>
+        /// Also called patio. Paved area between the house and garden for sitting and eating, etc.
+        /// </summary>
+        Terrace = 1 << 13,
+
+        /// <summary>
+        /// A room for reading, writing, studying in.
+        /// </summary>
+        Study = 1 << 14,
+
+        /// <summary>
+        /// An area with a wall or bars around it that is joined to the outside wall of a building on an upper level.
+        /// </summary>
+        Balcony = 1 << 15,
+
+        /// <summary>
+        /// Represents all house pasts together.
+        /// </summary>
+        All = int.MaxValue
+    }
+}
---
+++ sources/SomeData/HouseType.cs
@@ -0,0 +1,83 @@
+﻿// FlagCalculator
+// Copyright (C) 2017 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace DustInTheWind.SomeData
+{
+    /// <summary>
+    /// Represents a type of house.
+    /// </summary>
+    /// <remarks>
+    /// It is a public enum that does not have the <see cref="FlagsAttribute"/> on it.
+    /// FlagCalculator application should raise a warning, but still load this enum.
+    /// </remarks>
+    public enum HouseType
+    {
+        /// <summary>
+        /// Represents no house type.
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// A building which people, usually one family, live in.
+        /// </summary>
+        House,
+
+        /// <summary>
+        /// Describes a house that is not connected to any other building.
+        /// </summary>
+        DetachedHouse,
+
+        /// <summary>
+        /// A house that is semi-detached is one that is joined to another similar house on only one side.
+        /// </summary>
+        SemiDetachedHouse,
+
+        /// <summary>
+        /// In UK usually flat. A set of rooms for living in, especially on one floor of a building.
+        /// </summary>
+        Apartment,
+
+        /// <summary>
+        /// A house that is joined to the houses on either side of it by shared walls.
+        /// </summary>
+        TerracedHouse,
+
+        /// <summary>
+        /// A small house, usually in the countryside.
+        /// </summary>
+        Cottage,
+
+        /// <summary>
+        /// A house that has only one story/floor.
+        /// </summary>
+        Bungalow,
+
+        /// <summary>
+        /// A rented room which has a bed, table, chairs and somewhere to cook in it.
+        /// </summary>
+        Bedsit,
+
+        /// <summary>
+        /// A house usually in the countryside or near the sea, particularly in southern Europe, and often one which people can rent for a holiday.
+        /// </summary>
+        Villa,
+
+        /// <summary>
+        /// A holiday house or apartment which is owned by several different people, each of whom is able to use it for a particular period of the year.
+        /// </summary>
+        TimeShare
+    }
+}
