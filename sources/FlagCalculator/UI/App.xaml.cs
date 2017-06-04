21b786a507ab336317c2ea3776740638704fdd2c
lastunicorn <coldblackstar@yahoo.com>
2017-06-04 08:41:19 +0300

    moved style files into a separate directory.

--- sources/FlagCalculator/UI/App.xaml.cs
+++ sources/FlagCalculator/UI/App.xaml.cs
@@ -1,4 +1,20 @@
-﻿using System.Windows;
+﻿// FlagCalculator
+// Copyright (C) 2017 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Windows;
 
 namespace DustInTheWind.FlagCalculator.UI
 {
@@ -7,9 +23,5 @@ namespace DustInTheWind.FlagCalculator.UI
     /// </summary>
     public partial class App : Application
     {
-        private void FrameworkElement_OnRequestBringIntoView(object sender, RequestBringIntoViewEventArgs e)
-        {
-            e.Handled = true;
-        }
     }
 }
