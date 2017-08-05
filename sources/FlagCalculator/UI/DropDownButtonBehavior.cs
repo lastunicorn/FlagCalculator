eb3dcbff2c9705718588a1f655e9ed27cd4e1ec8
lastunicorn <coldblackstar@yahoo.com>
2017-08-05 09:37:56 +0300

    failed tentative to create a dropdown menu button.

---
+++ sources/FlagCalculator/UI/DropDownButtonBehavior.cs
@@ -0,0 +1,51 @@
+﻿// FlagCalculator
+// Copyright (C) 2017 Dust in the Wind
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Interactivity;
+
+namespace DustInTheWind.FlagCalculator.UI
+{
+    public class DropDownButtonBehavior : Behavior<Button>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.AddHandler(ButtonBase.ClickEvent, new RoutedEventHandler(AssociatedObject_Click), true);
+        }
+
+        private void AssociatedObject_Click(object sender, RoutedEventArgs e)
+        {
+            Button source = sender as Button;
+
+            if (source?.ContextMenu != null)
+            {
+                // If there is a drop-down assigned to this button, then position and display it 
+                //source.ContextMenu.PlacementTarget = source;
+                //source.ContextMenu.Placement = PlacementMode.Top;
+                source.ContextMenu.IsOpen = true;
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.RemoveHandler(ButtonBase.ClickEvent, new RoutedEventHandler(AssociatedObject_Click));
+        }
+    }
+}
