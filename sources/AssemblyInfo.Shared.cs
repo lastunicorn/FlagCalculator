debadd3eea748ee1accac7a2230b3dc6ebe4fb49
lastunicorn <coldblackstar@yahoo.com>
2017-08-30 17:29:21 +0300

    increased the version to 1.2.2

--- sources/AssemblyInfo.Shared.cs
+++ sources/AssemblyInfo.Shared.cs
@@ -36,6 +36,6 @@ using System.Reflection;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2.1.*")]
+[assembly: AssemblyVersion("1.2.2.*")]
 //[assembly: AssemblyFileVersion("1.0.0.*")]
 //[assembly: AssemblyInformationalVersion("1.0.0")]
