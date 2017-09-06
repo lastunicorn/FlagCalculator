e8d73d972cbdc618ea46dcbb4bc1fd2a86323fdf
lastunicorn <coldblackstar@yahoo.com>
2017-09-06 22:49:05 +0300

    updated the display of hexa values.

--- sources/FlagCalculator/UI/Converters/SmartNumberToStringConverter.cs
+++ sources/FlagCalculator/UI/Converters/SmartNumberToStringConverter.cs
@@ -15,6 +15,7 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Windows.Data;
@@ -42,16 +43,54 @@ namespace DustInTheWind.FlagCalculator.UI.Converters
             {
                 case NumericalBase.None:
                 case NumericalBase.Decimal:
-                case NumericalBase.Hexadecimal:
                     return smartNumber.ToString();
 
+                case NumericalBase.Hexadecimal:
+                    return RenderHexadecimal(smartNumber);
+
                 case NumericalBase.Binary:
+                    return RenderBinary(smartNumber);
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        private static object RenderHexadecimal(SmartNumber smartNumber)
+        {
+            string xamlContent = smartNumber.ToString();
+
+            List<string> result = new List<string>();
+            bool isBegin = true;
+
+            foreach (char c in xamlContent)
+            {
+                bool isDigit = char.IsDigit(c) || (c >= 'A' && c <= 'F');
+                
+                if (isBegin)
+                {
+                    if (isDigit && c != '0')
                     {
-                        string xamlContent = smartNumber.ToString();
+                        isBegin = false;
+                        result.Add($@"<Run Foreground=""Black"">{c}</Run>");
+                    }
+                    else
+                    {
+                        result.Add(c.ToString());
+                    }
+                }
+                else
+                {
+                    if (isDigit)
+                        result.Add($@"<Run Foreground=""Black"">{c}</Run>");
+                    else
+                        result.Add(c.ToString());
+                }
+            }
 
-                        xamlContent = xamlContent.Replace("1", @"<Run Foreground = ""Black"">1</Run>");
+            xamlContent = string.Join("", result);
 
-                        string xamlTextBlock = @"
+            string xamlTextBlock = @"
             <TextBlock
                 xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
                 Padding = ""5""
@@ -63,16 +102,36 @@ namespace DustInTheWind.FlagCalculator.UI.Converters
 
             </TextBlock>";
 
-                        xamlTextBlock = string.Format(xamlTextBlock, xamlContent);
+            xamlTextBlock = string.Format(xamlTextBlock, xamlContent);
 
-                        using (StringReader stringReader = new StringReader(xamlTextBlock))
-                        using (XmlReader xmlReader = XmlReader.Create(stringReader))
-                            return XamlReader.Load(xmlReader);
-                    }
+            using (StringReader stringReader = new StringReader(xamlTextBlock))
+            using (XmlReader xmlReader = XmlReader.Create(stringReader))
+                return XamlReader.Load(xmlReader);
+        }
 
-                default:
-                    throw new ArgumentOutOfRangeException();
-            }
+        private static object RenderBinary(SmartNumber smartNumber)
+        {
+            string xamlContent = smartNumber.ToString();
+
+            xamlContent = xamlContent.Replace("1", @"<Run Foreground = ""Black"">1</Run>");
+
+            string xamlTextBlock = @"
+            <TextBlock
+                xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation""
+                Padding = ""5""
+                VerticalAlignment = ""Center""
+                HorizontalAlignment = ""Stretch""
+                Foreground = ""Gray"">
+
+                {0}
+
+            </TextBlock>";
+
+            xamlTextBlock = string.Format(xamlTextBlock, xamlContent);
+
+            using (StringReader stringReader = new StringReader(xamlTextBlock))
+            using (XmlReader xmlReader = XmlReader.Create(stringReader))
+                return XamlReader.Load(xmlReader);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
