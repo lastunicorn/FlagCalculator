8ef9c6ba2bd6bef27dda3ec03c2a061b36a5acc3
lastunicorn <coldblackstar@yahoo.com>
2017-12-15 08:30:10 +0200

    started to add the tab move functionality - not working yet.

--- sources/FlagCalculator/UI/Views/MainWindow.xaml.cs
+++ sources/FlagCalculator/UI/Views/MainWindow.xaml.cs
@@ -15,6 +15,8 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace DustInTheWind.FlagCalculator.UI.Views
 {
@@ -26,6 +28,40 @@ namespace DustInTheWind.FlagCalculator.UI.Views
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void TabItem_PreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            TabItem tabItem = e.Source as TabItem;
+
+            if (tabItem == null)
+                return;
+
+            if (Mouse.PrimaryDevice.LeftButton == MouseButtonState.Pressed)
+            {
+                DragDrop.DoDragDrop(tabItem, tabItem, DragDropEffects.All);
+            }
+        }
+
+
+        private void TabItem_Drop(object sender, DragEventArgs e)
+        {
+            TabItem target = e.Source as TabItem;
+            TabItem source = e.Data.GetData(typeof(TabItem)) as TabItem;
+
+            if (!target.Equals(source))
+            {
+                //var tabControl = target.Parent as TabControl;
+                var tabControl = TabControl1;
+                int sourceIndex = tabControl.Items.IndexOf(source);
+                int targetIndex = tabControl.Items.IndexOf(target);
+
+                tabControl.Items.Remove(source);
+                tabControl.Items.Insert(targetIndex, source);
+
+                tabControl.Items.Remove(target);
+                tabControl.Items.Insert(sourceIndex, target);
+            }
         }
     }
 }
